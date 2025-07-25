b6332c025878c99e255ca8e0016f466ab7822b49
BWmagician <master10032spark@gmail.com>
2025-07-26 00:24:23 +0800

    fix some problems due to flex layout.

--- source/js/index.js
+++ source/js/index.js
@@ -2,7 +2,7 @@ let articleBox = document.getElementsByClassName("articleBox");
 let articleTitles = document.getElementsByClassName("articleTitle");
 if(isMobile()){
     for(let i = 0; i < articleBox.length; ++i){
-        articleBox[i].setAttribute('style',`width:100%;font-size: 3rem;margin-top:${2}svh;margin-bottom:${2}svh;padding-bottom:${2}svh;`);
+        articleBox[i].setAttribute('style',`width:90svw;font-size: 3rem;margin-top:${2}svh;margin-bottom:${2}svh;padding-bottom:${2}svh;`);
     }
     for(let i = 0; i < articleTitles.length; ++i){
         articleTitles[i].setAttribute('style',`font-size: 4rem`);
